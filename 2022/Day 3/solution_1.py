fbac81bed2e26ab2781cc69a35ff2eae16cd2834
David-Essaadi <davidessaadi@gmail.com>
2022-12-03 18:14:36 +0100

    Small adjustments

--- 2022/Day 3/solution_1.py
+++ 2022/Day 3/solution_1.py
@@ -1,8 +1,8 @@
 import numpy as np
 import string
 
-def priority(item):
-    return string.ascii_letters.index(item) + 1
+def to_priority(items):
+    return list(map(lambda item: string.ascii_letters.index(item) + 1, items))
 
 with open('input.txt') as file:
-    print(sum(map(lambda items: np.intersect1d(list(map(priority, items[:len(items) // 2])), list(map(priority, items[len(items) // 2:])))[0], file.read().strip().splitlines())))
+    print(sum(map(lambda items: np.intersect1d(items[:len(items) // 2], items[len(items) // 2:])[0], map(to_priority, file.read().strip().splitlines()))))
--- 2022/Day 3/solution_2.py
+++ 2022/Day 3/solution_2.py
@@ -1,9 +1,9 @@
 import numpy as np
 import string
 
-def priority(item):
-    return string.ascii_letters.index(item) + 1
+def to_priority(items):
+    return list(map(lambda item: string.ascii_letters.index(item) + 1, items))
 
 with open('input.txt') as file:
-    print(sum(map(lambda items: np.intersect1d(list(map(priority, items[0])), np.intersect1d(list(map(priority, items[1])), list(map(priority, items[2]))))[0], list(zip(*(iter(file.read().strip().splitlines()),) * 3)))))
+    print(sum(map(lambda items: np.intersect1d(items[0], np.intersect1d(items[1], items[2]))[0], list(zip(*(iter(map(to_priority, file.read().strip().splitlines())),) * 3)))))
     
